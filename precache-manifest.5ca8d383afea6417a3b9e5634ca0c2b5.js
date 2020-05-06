f2aa9e7df4dc6b873585c3facdbdd89114602e5a
Salem <Salem.dahmani12@hotmail.fr>
2020-05-07 01:23:26 +0200

    Updates

---
+++ precache-manifest.5ca8d383afea6417a3b9e5634ca0c2b5.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "176752579292331ae2fe5695ce65c3d1",
+    "url": "/Friends_Project/index.html"
+  },
+  {
+    "revision": "08bc1b4a1bad6bf2c5a7",
+    "url": "/Friends_Project/static/css/2.036b41f9.chunk.css"
+  },
+  {
+    "revision": "d5becc9c550fedf7ac24",
+    "url": "/Friends_Project/static/css/main.93c43de1.chunk.css"
+  },
+  {
+    "revision": "08bc1b4a1bad6bf2c5a7",
+    "url": "/Friends_Project/static/js/2.8eb3436a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Friends_Project/static/js/2.8eb3436a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5becc9c550fedf7ac24",
+    "url": "/Friends_Project/static/js/main.3af68aaf.chunk.js"
+  },
+  {
+    "revision": "0d52836f8c2200600523",
+    "url": "/Friends_Project/static/js/runtime-main.22fc45d6.js"
+  },
+  {
+    "revision": "4b93a344061c833643ec2e4cb5725d6f",
+    "url": "/Friends_Project/static/media/boutriga.4b93a344.jpg"
+  },
+  {
+    "revision": "9e15ad8a96ada8a109b42ebbc4506e30",
+    "url": "/Friends_Project/static/media/ghazi.9e15ad8a.jpg"
+  },
+  {
+    "revision": "492505385941afdc437de7c5b9c7158d",
+    "url": "/Friends_Project/static/media/hedi.49250538.jpg"
+  },
+  {
+    "revision": "b5c7ba6f5d53b4925de2856a4bc4b749",
+    "url": "/Friends_Project/static/media/iheb.b5c7ba6f.jpg"
+  },
+  {
+    "revision": "48256990845299c4a03eeafc6baf6c01",
+    "url": "/Friends_Project/static/media/mahmoud.48256990.jpg"
+  },
+  {
+    "revision": "125f8761c56634557d9ce476cfbf8814",
+    "url": "/Friends_Project/static/media/marweni.125f8761.jpg"
+  },
+  {
+    "revision": "5821ae09b1b19c8a249e71002d2e07de",
+    "url": "/Friends_Project/static/media/nejib.5821ae09.jpg"
+  },
+  {
+    "revision": "b37cf6d7c4d012bddc655926ec3e3663",
+    "url": "/Friends_Project/static/media/petit.b37cf6d7.jpg"
+  },
+  {
+    "revision": "4e6102733d55ede33367f93145aad94d",
+    "url": "/Friends_Project/static/media/salem.4e610273.jpg"
+  },
+  {
+    "revision": "82f9508d77b4810caaf03e95ceb6b5ba",
+    "url": "/Friends_Project/static/media/semah.82f9508d.jpg"
+  },
+  {
+    "revision": "d3faa9378c24bac2b9e9d1ed68b7cdd1",
+    "url": "/Friends_Project/static/media/weal.d3faa937.jpg"
+  }
+]);
